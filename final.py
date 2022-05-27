5764631547e260d4f7438905c868ce5b23df46f0
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-05-27 14:50:50 +0200

    Update final.py

--- final.py
+++ final.py
@@ -12,7 +12,7 @@ st.title("Project_Name")
 st.write("Please insert a relative path of the PDF file. Note that the PDF file has to be saved in and copied from the same folder in GitHub, /n where  the .py file is. Try for example: CORE_INTENTIONAL_FEATURES_IN_THE_SYNTACT.pdf")
 
 relative_path = st.file_uploader("Please choose a file")
-stringio = StringIO(relative_path.getvalue().decode("JVBERi0xLjUKJYCBgoMKMSAwIG9iago8PC9GaWx0ZXIvRmxhdGVEZWNvZGUvRmlyc3QgMTQxL04gMjAvTGVuZ3=="))
+stringio = StringIO(relative_path.getvalue().decode("UTF-8"))
 st.write(stringio)
 
 # string_data = stringio.read()

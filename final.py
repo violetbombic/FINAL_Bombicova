d0ff61213fd8fe523bd473479433b99032b603ac
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-05-27 14:58:32 +0200

    Update final.py

--- final.py
+++ final.py
@@ -12,18 +12,18 @@ st.title("Project_Name")
 st.write("Please insert a relative path of the PDF file. Note that the PDF file has to be saved in and copied from the same folder in GitHub, /n where  the .py file is. Try for example: CORE_INTENTIONAL_FEATURES_IN_THE_SYNTACT.pdf")
 
 relative_path = st.file_uploader("Please choose a file")
-stringio = StringIO(relative_path.getvalue().decode("ISO-8859-1"))
-st.write(stringio)
+# stringio = StringIO(relative_path.getvalue().decode("ISO-8859-1"))
+# st.write(stringio)
 
-string_data = stringio.read()
-st.write(string_data)
+# string_data = stringio.read()
+# st.write(string_data)
 
 # relative_path = st.text_input('Give me a relative path to PDF file (without the quotes): ')
 
 # def OpenPDF(relative_path):
 
   
-
+st.write(type(relative_path)
 # creating a pdf file object 
 objfile = open(relative_path, 'rb') 
 

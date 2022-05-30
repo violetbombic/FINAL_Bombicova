364ef4613a65831e98e9b097a62fc82ec002f360
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-05-30 16:03:13 +0200

    Update final.py

--- final.py
+++ final.py
@@ -43,21 +43,21 @@ if relative_path is not None:
     pageObj = pdfReader.getPage(1) 
 
     # extracting text from page 
-    text = st.text_area(pageObj.extract_text)
+    text = st.write(pageObj.extract_text)
 
 
     # closing the pdf file object 
     objfile.close()
 
-    page_content=""
-    with open(relative_path,'rb') as pdf_file:
-        read_pdf = PyPDF2.PdfFileReader(pdf_file)
-        number_of_pages = read_pdf.getNumPages()
-        for page_number in range(number_of_pages):
-            page = read_pdf.getPage(page_number)
-            page_content += page.extract_text()
+#     page_content=""
+#     with open(relative_path,'rb') as pdf_file:
+#         read_pdf = PyPDF2.PdfFileReader(pdf_file)
+#         number_of_pages = read_pdf.getNumPages()
+#         for page_number in range(number_of_pages):
+#             page = read_pdf.getPage(page_number)
+#             page_content += page.extract_text()
  
-        st.write(page_content)
+#         st.write(page_content)
     
 # OpenPDF(relative_path)
 
